fecfbb082b85f888c553ee05ab171aa681c2e9c9
Daniel Irvine <dirvine@8thlight.com>
2014-10-20 15:24:31 +0100

    simplified numbers-or-marks method

--- src/clj_ttt/cli_game.clj
+++ src/clj_ttt/cli_game.clj
@@ -4,14 +4,13 @@
 (defn output-board-line [square-strings]
   (str (clojure.string/join " " square-strings) "\n"))
 
-(defn number-or-mark [mark index playable-squares]
+(defn number-or-mark [index mark playable-squares]
   (if (some #{index} playable-squares)
     (str (inc index))
     mark))
 
 (defn numbers-or-marks [board]
-  (let [playable-squares (playable-squares board)]
-    (map-indexed (fn [index mark] (number-or-mark mark index playable-squares)) (seq board))))
+  (map-indexed #(number-or-mark %1 %2 (playable-squares board)) board))
 
 (defn board-string [board size]
   (apply str (map output-board-line (partition size (numbers-or-marks board)))))
