1ae71ffa10e41c2e9614a9749f5c906d94c40594
Daniel Irvine <dirvine@8thlight.com>
2014-10-20 12:11:48 +0100

    reworked functions to use strings, which simplifies tests somewhat (at the moment...)

--- spec/clj_ttt/board_spec.clj
+++ spec/clj_ttt/board_spec.clj
@@ -3,22 +3,22 @@
             [clj-ttt.core :refer :all]))
 
 (defn create-board [size]
-  (vec (replicate (* size size) "-")))
+  (apply str (replicate (* size size) "-")))
 
 (defn play-move [board square mark]
-  (assoc board square mark))
+  (apply str (assoc (vec board) square mark)))
 
 (describe "creating an empty board"
           (it "creates an empty board string"
-              (should= "---------" (apply str (create-board 3)))
+              (should= "---------" (create-board 3))
           )
           (it "creates a 4x4 board string"
-              (should= "----------------" (apply str (create-board 4)))
+              (should= "----------------" (create-board 4))
           )
 )
 
 (describe "playing a move"
           (it "plays an X"
-              (should= "--X------" (apply str (play-move (vec "---------") 2 "X"))))
+              (should= "--X------" (play-move "---------" 2 "X")))
           )
 
