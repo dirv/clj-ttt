66be82d1157b86c9cd6b7e588681fbc463638eff
Daniel Irvine <dirvine@8thlight.com>
2014-10-20 13:09:18 +0100

    winning rows

--- spec/clj_ttt/board_spec.clj
+++ spec/clj_ttt/board_spec.clj
@@ -13,20 +13,29 @@
        (apply str (assoc bv square mark))
        board)))
 
+(defn won? [board]
+  (some 
+  (fn [sub] (= "XXX" sub))
+    (list (subs board 0 3)
+  (subs board 3 6)
+  (subs board 6 9)))
+    )
+
 (describe "creating an empty board"
           (it "creates an empty board string"
-              (should= "---------" (create-board 3))
-          )
+              (should= "---------" (create-board 3)))
           (it "creates a 4x4 board string"
-              (should= "----------------" (create-board 4))
-          )
-)
+              (should= "----------------" (create-board 4))))
 
 (describe "playing a move"
           (it "plays an X"
-              (should= "--X------" (play-move "---------" 2 "X"))
-          )
+              (should= "--X------" (play-move "---------" 2 "X")))
           (it "only plays a move if square is empty"
-              (should= "X" (play-move "X" 0 "O")))
-          )
+              (should= "X" (play-move "X" 0 "O"))))
+
+(describe "winning a game"
+          (it "wins a row game"
+              (should= true (won? "XXX------")))
+          (it "wins a middle row game"
+              (should= true (won? "---XXX---"))))
 
