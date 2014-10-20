babdf84a65fcfab8435b5590c4151db235b652aa
Daniel Irvine <dirvine@8thlight.com>
2014-10-20 12:08:42 +0100

    play move

--- spec/clj_ttt/board_spec.clj
+++ spec/clj_ttt/board_spec.clj
@@ -5,6 +5,9 @@
 (defn create-board [size]
   (vec (replicate (* size size) "-")))
 
+(defn play-move [board square mark]
+  (assoc board square mark))
+
 (describe "creating an empty board"
           (it "creates an empty board string"
               (should= "---------" (apply str (create-board 3)))
@@ -13,4 +16,9 @@
               (should= "----------------" (apply str (create-board 4)))
           )
 )
+
+(describe "playing a move"
+          (it "plays an X"
+              (should= "--X------" (apply str (play-move (vec "---------") 2 "X"))))
+          )
 
