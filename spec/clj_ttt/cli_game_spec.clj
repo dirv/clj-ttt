9eff192b4ce636e29bc5ac781f8cf0637f16ad5b
Daniel Irvine <dirvine@8thlight.com>
2014-10-20 15:13:14 +0100

    output board with played squares

--- spec/clj_ttt/cli_game_spec.clj
+++ spec/clj_ttt/cli_game_spec.clj
@@ -7,4 +7,8 @@
           (it "outputs an empty game board"
               (should= "1 2 3\n" (board-string "---" 3)))
           (it "splits game board by newline"
-              (should= "1 2 3\n4 5 6\n" (board-string "------" 3))))
+              (should= "1 2 3\n4 5 6\n" (board-string "------" 3)))
+          (it "shows mark for played square"
+              (should= "X\n" (board-string "X" 1)))
+          (it "shows mixed game"
+              (should= "X O X\n4 5 6\n" (board-string "XOX---" 3))))
--- src/clj_ttt/cli_game.clj
+++ src/clj_ttt/cli_game.clj
@@ -2,10 +2,19 @@
   (:require [clj-ttt.board :refer :all]))
 
 (defn output-board-line [square-strings]
-  (str (clojure.string/join " " (map #(str (inc %)) square-strings)) "\n"))
+  (str (clojure.string/join " " square-strings) "\n"))
 
-(defn board-string [board size]
-  (let [squares (playable-squares board)]
-    (apply str (map output-board-line (vec (partition size squares))))
+(defn number-or-mark [mark index playable-squares]
+  (if (some #{index} playable-squares)
+    (str (inc index))
+    mark
     ))
 
+(defn numbers-or-marks [board]
+  (let [playable-squares (playable-squares board)]
+    (map-indexed (fn [index mark] (number-or-mark mark index playable-squares)) (seq board))))
+
+(defn board-string [board size]
+  (let [square-strings (numbers-or-marks board)]
+    (apply str (map output-board-line (vec (partition size square-strings))))
+    ))
