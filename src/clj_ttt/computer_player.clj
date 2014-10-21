785f1c809abb4e632c6281ab7a00dc4acbb16805
Daniel Irvine <dirvine@8thlight.com>
2014-10-21 23:42:07 +0100

    inline method

--- src/clj_ttt/computer_player.clj
+++ src/clj_ttt/computer_player.clj
@@ -12,26 +12,24 @@
 (defn initial-score [alpha beta]
   {:score alpha :board nil :alpha alpha :beta beta})
 
-(declare play)
-
-(defn best-move [current board depth]
-  (let
-    [next-player (next-player board)
-     new-move (play board next-player (dec depth) (- (:beta current)) (- (:alpha current)))
-     new-score (- (:score new-move))]
-    (if (> new-score (:score current))
-      {:score new-score :board board :alpha new-score :beta (:beta current)}
-      current)))
-
 (defn play [board mark depth alpha beta]
   (if (or (= 0 depth) (finished? board))
     {:score (- (score board depth)) :board board :alpha alpha :beta beta}
-    (reduce (fn [current board]
-              (if (>= (:alpha current) (:beta current))
-                (reduced current)
-                (best-move current board depth)))
+
+    (let [next-player (last-player board)
+          next-depth (dec depth)]
+      (reduce (fn [best-move board]
+              (if (>= (:alpha best-move) (:beta best-move))
+                (reduced best-move)
+                (let [new-alpha (- (:beta best-move))
+                      new-beta (- (:alpha best-move))
+                      new-move (play board next-player next-depth new-alpha new-beta)
+                      new-score (- (:score new-move))]
+                  (if (> new-score (:score best-move))
+                    {:score new-score :board board :alpha new-score :beta (:beta best-move)}
+                    best-move))))
             (initial-score alpha beta)
-            (all-moves board mark (playable-squares board)))))
+            (all-moves board mark (playable-squares board))))))
 
 (defn choose-next-move [board mark]
   (:board (play
