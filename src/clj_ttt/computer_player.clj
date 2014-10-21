4291f4542366fb69546077491771c8f60bae5375
Daniel Irvine <dirvine@8thlight.com>
2014-10-21 23:18:48 +0100

    finish alpha beta pruning

--- src/clj_ttt/computer_player.clj
+++ src/clj_ttt/computer_player.clj
@@ -1,36 +1,42 @@
 (ns clj-ttt.computer-player
   (:require [clj-ttt.board :refer :all]))
 
-(def ^:const no-score {:score -1 :board nil :alpha -1000 :beta 1000})
+(def ^:const infinity 1000)
 
 (defn score [board depth]
-  (if (won? board)
-    depth
-    0))
+  (if (won? board) depth 0))
 
 (defn all-moves [board mark squares]
   (map #(play-move board % mark) squares))
+
+(defn initial-score [alpha beta]
+  {:score alpha :board nil :alpha alpha :beta beta})
 
 (declare play)
 
 (defn best-move [current board depth]
   (let
     [next-player (next-player board)
-     new-move (play board next-player (dec depth))
+     new-move (play board next-player (dec depth) (- (:beta current)) (- (:alpha current)))
      new-score (- (:score new-move))]
     (if (> new-score (:score current))
-      {:score new-score :board board :alpha -1000 :beta 1000}
+      {:score new-score :board board :alpha new-score :beta (:beta current)}
       current)))
 
-(defn play [board mark depth]
+(defn play [board mark depth alpha beta]
   (if (or (= 0 depth) (finished? board))
-    {:score (- (score board depth)) :board board}
-    (reduce (fn [current board] 
+    {:score (- (score board depth)) :board board :alpha alpha :beta beta}
+    (reduce (fn [current board]
               (if (>= (:alpha current) (:beta current))
                 (reduced current)
                 (best-move current board depth)))
-            no-score
+            (initial-score alpha beta)
             (all-moves board mark (playable-squares board)))))
 
 (defn choose-next-move [board mark]
-  (:board (play board mark (inc (count (playable-squares board))))))
+  (:board (play
+            board
+            mark
+            (inc (count (playable-squares board)))
+            (- infinity)
+            infinity)))
